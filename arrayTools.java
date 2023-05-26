5cfe261ad12d187905df182f35d0fb760d69f766
Soheil <soheilrajabali@gmail.com>
2023-05-25 23:37:15 -0400

    Completed the users ability to select their theme

--- arrayTools.java
+++ arrayTools.java
@@ -3,7 +3,7 @@ import arc.*;
 public class arrayTools {
     public static int arrayLength(TextInputFile txtFile){
         int intCount = 0;
-        
+
         while(txtFile.eof() == false){
             txtFile.readLine();
             intCount++;
@@ -17,6 +17,19 @@ public class arrayTools {
 
         for(intCount1 = 0; intCount1 < intCount; intCount1++){
             strArray[intCount1] = txtFile.readLine();
+        }
+        txtFile.close();
+        return strArray;
+    }
+    public static String[][] loadTheme(TextInputFile txtFile, int intCount){
+        int intCount1;
+        int intRandInt;
+        String strArray[][] = new String[intCount][2];
+
+        for(intCount1 = 0; intCount1 < intCount; intCount1++){
+            intRandInt = (int)Math.floor(Math.random()*(intCount));
+            strArray[intCount1][0] = txtFile.readLine();
+            strArray[intCount1][1] = String.valueOf(intRandInt);
         }
         txtFile.close();
         return strArray;
--- main.java
+++ main.java
@@ -1,27 +1,62 @@
+/*
+ * Soheil Rajabali
+ * Hangman
+ * V2.0
+ */
+
 import arc.*;
 
 public class main{
     public static void main(String[] args){
-        //Initializes the Console
+        //Initializes varables
         Console con = new Console();
-
-        //Variable Definitions
+        String strThemeChoice[][];
+        String strChoice;
         int intThemeLength;
+
+        //Gets the users theme choice
+        strChoice = themeSelect(con);
+        TextInputFile txtThemeChoice = new TextInputFile("Themes/"+strChoice);
+
+        //Loads their theme choice into an array
+        intThemeLength = arrayTools.arrayLength(txtThemeChoice);
+        strThemeChoice = new String[intThemeLength][2];
+        txtThemeChoice.close();
+        txtThemeChoice = new TextInputFile("Themes/"+strChoice);
+        strThemeChoice = arrayTools.loadTheme(txtThemeChoice, intThemeLength);
+        txtThemeChoice.close();
+
+        for(int intCount1 = 0;intCount1 < intThemeLength; intCount1++){
+            System.out.println(strThemeChoice[intCount1][0] + " - " + strThemeChoice[intCount1][1]);
+        }
+    }
+    public static String themeSelect(Console con){
+        //Variable Definitions
+        String strChoice;
         String strThemes[];
         int intCount;
+        int intThemeLength;
         TextInputFile txtThemes = new TextInputFile("Themes.txt");
 
-        //loads the themes to an array
+        //Loads the themes to an array
         intThemeLength = arrayTools.arrayLength(txtThemes);
         strThemes = new String[intThemeLength];
         txtThemes.close();
         txtThemes = new TextInputFile("Themes.txt");
         strThemes = arrayTools.load1DArray(txtThemes, intThemeLength);
+        txtThemes.close();
 
         //Prints out all the theme options
         con.println("What theme would you like to select? Your options are:");
         for(intCount = 0; intCount < intThemeLength;intCount++){
             con.println(strThemes[intCount]);
         }
+
+        //Gets the users input
+        con.print("> ");
+        strChoice = con.readLine();
+        strChoice = strChoice+".txt";
+        
+        return strChoice;
     }
 }
